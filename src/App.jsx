99707b7661b0b846b4eff6f6883ddf1cce8b671d
Vincent <vincent.douchin@gmail.com>
2022-07-14 12:02:37 +0200

    added date picker and confirmation modal

--- src/App.jsx
+++ src/App.jsx
@@ -2,52 +2,43 @@ import { useState } from 'react';
 import { Event } from '@mui/icons-material';
 import { useNavigate } from '@tanstack/react-location'
 import { Container, Button, Typography, Toolbar, Box, AppBar, Card, Stack, CardActionArea, CardMedia, Collapse, CardContent, } from '@mui/material';
-import salle1 from '../public/saint-laurent1.jpg'
-import salle2 from '../public/saint-patrice3.jpg'
-import salle3 from '../public/salle_la_comete_ok.jpg'
+import salles from './salles'
 function App() {
 	const navigate = useNavigate()
-	const salles = [
-		{ id: 0, name: 'Gymnase', img: salle1, adress: '5 route du Parc', surface: '400m²', capacity: 20 },
-		{ id: 1, name: 'Mairie', img: salle2, adress: '1 rue du Castel', surface: '200m²', capacity: 50 },
-		{ id: 2, name: 'Eglise', img: salle3, adress: '15 rue du Castel', surface: '300m²', capacity: 30 }
-	]
+
 	const [expanded, setExpanded] = useState()
 	return (
-		<Box sx={{ flexGrow: 1 }}>
 
-			<Container maxWidth="sm" style={{ marginTop: '1em' }} >
-				<Stack spacing={2}>
-					{salles.map(salle => <Card key={salle.id}>
-						<CardActionArea
-							onClick={() => setExpanded(expanded === salle.id ? null : salle.id)}
-						>
-							<CardContent>
-								<Typography style={{ margin: '1em' }} variant="h6" component="div">
-									{salle.name}
-								</Typography>
-								<CardMedia
-									component="img"
-									height="140"
-									image={salle.img}
-									alt={salle.name}
-								/>
-							</CardContent>
-						</CardActionArea>
-						<Collapse in={expanded === salle.id}>
-							<CardContent>
-								<Stack spacing={1}>
-									<Typography >Adresse : {salle.adress}</Typography >
-									<Typography >Surface : {salle.surface}</Typography >
-									<Typography >Capacité : {salle.capacity} personnes</Typography >
-									<Button onClick={() => navigate({ to: salle.id })} startIcon={<Event />}>Faire une réservation</Button>
-								</Stack >
-							</CardContent>
-						</Collapse>
-					</Card >)}
-				</Stack>
-			</Container>
-		</Box>
+		<Stack spacing={2}>
+			{salles.map(salle => <Card key={salle.id}>
+				<CardActionArea
+					onClick={() => setExpanded(expanded === salle.id ? null : salle.id)}
+				>
+					<CardContent>
+						<Typography style={{ margin: '1em' }} variant="h6" component="div">
+							{salle.name}
+						</Typography>
+						<CardMedia
+							component="img"
+							height="140"
+							image={salle.img}
+							alt={salle.name}
+						/>
+					</CardContent>
+				</CardActionArea>
+				<Collapse in={expanded === salle.id}>
+					<CardContent>
+						<Stack spacing={1}>
+							<Typography >Adresse : {salle.adress}</Typography >
+							<Typography >Surface : {salle.surface}</Typography >
+							<Typography >Capacité : {salle.capacity} personnes</Typography >
+							<Button onClick={() => navigate({ to: salle.id })} startIcon={<Event />}>Faire une réservation</Button>
+						</Stack >
+					</CardContent>
+				</Collapse>
+			</Card >)}
+		</Stack>
+
 	)
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ReactLocation, Router, Outlet } from '@tanstack/react-location'
+import { Box, Container } from '@mui/material'
 import Header from './Header'
 import routes from './routes'
 import './index.css'
@@ -8,12 +9,18 @@ const reactLocation = new ReactLocation()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header />
+
     <Router
       location={reactLocation}
       routes={routes}
     >
-      <Outlet />
+      <Header />
+      <Box sx={{ flexGrow: 1 }}>
+
+        <Container maxWidth="sm" style={{ marginTop: '1em' }} >
+          <Outlet />
+        </Container >
+      </Box >
     </Router>
   </React.StrictMode>
 )
